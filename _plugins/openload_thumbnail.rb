12729a00fbed963d0c4202c075bdc83eeefdb5ea
Sebastian Winkler <sekl@slmn.de>
2017-04-28 12:57:45 +0200

    adds experimental thumbnails

---
+++ _plugins/openload_thumbnail.rb
@@ -0,0 +1,21 @@
+require 'net/http'
+
+module Jekyll
+  class RenderTimeTag < Liquid::Tag
+
+    def initialize(tag_name, variables, tokens)
+      super
+    end
+
+    def render(context)
+        @openload_id = context[@markup.strip]
+
+        uri = URI('https://slmn.de/teamidealsubs-openload-thumbnail-proxy.php?file_id=' + @openload_id)
+        response = Net::HTTP.get(uri)
+
+        "#{response}"
+    end
+  end
+end
+
+Liquid::Template.register_tag('openload_thumbnail', Jekyll::RenderTimeTag)
