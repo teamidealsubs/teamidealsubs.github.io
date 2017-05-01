9da93109ef0c8291ab9055bf23ced2c942d2e36d
Sebastian Winkler <sekl@slmn.de>
2017-05-01 21:09:07 +0200

    adds sleep to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+sleep 5
+
 curl -X DELETE "https://api.cloudflare.com/client/v4/zones/$CF_ZONE_ID/purge_cache" -H "X-Auth-Email: $CF_AUTH_EMAIL" -H "X-Auth-Key: $CF_AUTH_KEY" -H "Content-Type: application/json" --data '{"purge_everything":true}'
